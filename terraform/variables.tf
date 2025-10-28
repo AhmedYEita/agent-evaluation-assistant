9c716ea8f8655c13a33c102b93ed5e8a9b0ad074
ahmedyasser.eita <ahmedyasser.eita@datatonic.com>
2025-10-28 12:49:46 -0600

    remove unused variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,14 +15,3 @@ variable "dataset_retention_days" {
   default     = 90
 }
 
-variable "enable_trace_sampling" {
-  description = "Enable sampling for Cloud Trace (reduces costs)"
-  type        = bool
-  default     = true
-}
-
-variable "trace_sample_rate" {
-  description = "Sampling rate for traces (0.0 to 1.0)"
-  type        = number
-  default     = 1.0
-}
